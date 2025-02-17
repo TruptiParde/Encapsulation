0471488f06258afc33030fba0af43db79ba7b167
TruptiParde <truptiparde1999@gmail.com>
2025-02-17 16:53:29 +0530

    addd

---
+++ Shopping/Items.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation.Shopping
+{
+    public class Items
+    {
+        public string Name { get; set; }
+        public long Price { get; set; }
+        public string Brand { get; set; }
+        public int Count { get; set; }
+        public string Size { get; set; }
+
+        public Items (string name, long price, string brand, int count, string size)
+        {
+            Name = name;
+            Price = price;
+            Brand = brand;
+            Count = count;
+            Size = size;
+        }
+    }
+}
---
+++ Shopping/ShoppingCart.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation.Shopping
+{
+    public class ShoppingCart
+    {
+        private List<Items> Item;
+        
+
+        public ShoppingCart()
+        {
+            Item = new List<Items>();
+        }
+
+        public void AddItem(string name, long price, string brand, int count, string size)
+        {
+            Item.Add(new Items(name, price));
+            Console.WriteLine($"Added: {name} - {price} to the cart.");
+        }
+
+        public long CalculateTotal()
+        {
+            long total = 0;
+            foreach (var item in Item)
+            {
+                total += item.Price;
+            }
+            return total;
+        }
+        public void GetCartDetails()
+        {
+            Console.WriteLine("Cart Details: ");
+            foreach (var  item in Item)
+            {
+                Console.WriteLine($"Item: {item.Name}, Price: {item.Price}");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ShoppingCart cart = new ShoppingCart();
+
+            cart.AddItem("laptop", 50000);
+            cart.AddItem("headphone", 20000);
+
+            cart.GetCartDetails();
+            Console.WriteLine($"Total Price: {cart.CalculateTotal()}");
+        }
+    }
+}
